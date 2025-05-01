9612e95ec4c1617b08787e41e350806268cc4478
CachedaCodes <CachedaCodes@gmail.com>
2025-05-01 11:49:29 +0200

    feat(tests): configure Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+
+export default defineVitestConfig({
+    test: {
+        environment: 'nuxt',
+        include: ['**/__test__/**/*.spec.{js,ts,jsx,tsx}'],
+    }
+})
